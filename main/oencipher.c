e412cde1c316ccbb4603feacd92ce5b2159a33f4
Alexander Oelkers <alexoelkers@gmail.com>
2023-10-14 16:22:06 +1000

    progressed encipher function

--- main/oencipher.c
+++ main/oencipher.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include "string.h"
 #include "stdlib.h"
 
 #define KEYSIZE 64
 
 void main()
 {
-    FILE* rawfile;
+    FILE *rawfile;
     char ch;
 
     rawfile = fopen("test.txt", "r");
 
-    if (NULL == rawfile){
+    if (NULL == rawfile)
+    {
         printf("The file coult not be opened.\n");
     }
 
     printf("the contents of this file are: \n");
 
-    while ((ch = fgetc(rawfile)) != EOF){
+    while ((ch = fgetc(rawfile)) != EOF)
+    {
         printf("%c", ch);
     }
     printf("\n");
@@ -24,19 +27,42 @@ void main()
     fclose(rawfile);
 }
 
-char* encipher(const char* plaintext, // 64 bit block
-               const char** keySchedule,         
-               const char* (*roundFunctionPtr)(char* substring, char* subkey))
+char *encipher(const char *plaintext,   // 64 bit block
+               const char **keySchedule // 4 keys
+               )                        // encipher plaintext using keySchedule
 {
-    char* leftString, rightString;
-    char* roundKey;
+    char ciphertext[KEYSIZE];
+    char *leftString, *rightString;
+    char *roundKey;
+
+    strcpy(ciphertext, plaintext);
 
     leftString = plaintext;
     rightString = plaintext + KEYSIZE / 2;
 
-    for (int i = 0; ((roundKey = *keySchedule++) != '\0'); i++) {
-            RightString = leftString
+    for (int i = 0; ((roundKey = *keySchedule++) != '\0'); i++)
+    {
+        leftString = rightString;
+        rightString = stringXOR(leftString, roundFunction(rightString, roundKey));
     }
+    return;
+}
 
+char *roundFunction(char *substring, // 32 bit block
+                    char *roundKey)
+{
+    return;
+}
 
+char *stringXOR(char *a, char *b)
+{
+    char *output;
+    char *cp = a;
+    int len = 0;
+
+    for (int i = 0; *cp++ != EOF; len++)
+    {
+    } // determine the length of the string
+
+    return;
 }
